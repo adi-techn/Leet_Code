28213ea327ff1d1c96e28c56066ea6385314a75f
Adi_Tech <adityasinghxa07@gmail.com>
2025-03-12 16:46:41 +0530

    Add files via upload

---
+++ lc_1047.java
@@ -0,0 +1,27 @@
+
+import java.util.Stack;
+public class lc_1047{
+    public static String removeStars(String s) {
+        StringBuilder sb=new StringBuilder();
+        Stack<Character> stack=new Stack<>();
+        stack.push(s.charAt(0));
+        int i=1;
+        while(i<s.length()){
+            char ch=s.charAt(i);
+            if(!stack.isEmpty() && ch==stack.peek()){
+                stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+            i++;
+        }
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(removeStars("azxxzy"));
+    }
+}
