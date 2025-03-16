4e903c263b0bfbb1b6e7e3698f97ce374feef350
Adi_Tech <adityasinghxa07@gmail.com>
2025-03-16 13:44:45 +0530

    Add files via upload

---
+++ lc_118.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc_118 {
+     public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            int num = 1; 
+            for (int j = 0; j <= i; j++) {
+                row.add(num);
+                num = num * (i - j) / (j + 1); 
+            }
+            triangle.add(row);
+        }
+        return triangle;
+     }
+     public static void main(String[] args) {
+          System.out.println(generate(5));
+     }
+}
