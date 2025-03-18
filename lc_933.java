9dde3bfd2aad65f66c22dc8bad48ed1b31a198ca
Adi_Tech <adityasinghxa07@gmail.com>
2025-03-19 01:21:29 +0530

    Add files via upload

---
+++ lc_933.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class lc_933 {
+     static class RecentCounter {
+          private Queue<Integer> q;
+          public RecentCounter() {
+               q=new LinkedList<>();
+          }
+    
+          public int ping(int t) {
+               q.add(t);
+               while(q.peek() < t - 3000){
+                    q.poll();
+               }    
+               return q.size();
+          }
+     }
+     public static void main(String[] args) {
+          RecentCounter rc=new RecentCounter();
+          System.out.println(rc.ping(1));
+          System.out.println(rc.ping(100));
+          System.out.println(rc.ping(3001));
+          System.out.println(rc.ping(3002));
+     }
+}
